e3326824faa164fc0eeff0e75cc2ab09e619bb8f
Bryan Alcorn <alcorn.bryan@gmail.com>
2016-11-27 22:38:30 -0800

    PCA: did pca and principal component analysis

--- code/scripts/pca_analysis.R
+++ code/scripts/pca_analysis.R
@@ -1,45 +1,63 @@
-# Purpose of this file: Find the variables we want to use as predictors for post graduation income
+# Purpose of this file: Use PCA 
 # This helps with dimension reduction and reducing the risk of multicollinearity
 
-require(pls)
-set.seed (1000)
 
-# We will begin our analysis on 2015 data 
-
-full_2015 <- read.csv("../../data/MERGED2014_15_PP.csv")
+#Begin Analysis on the training data
 training_data <- read.csv("../../data/training_data.csv")
-post_earning <- read.csv("../../data/post_earning.csv")
 
-#I will run PCA on a couple different potential predictors to test out different parameters and find the predictor I want to use
-#The predictor will be part of the dataset unemprate
 
-#unemployment rate
-pcr_model <- pcr(UNEMP_RATE~., data = training_data, scale = TRUE, validation = "CV")
+#unemployment rate, this is our first indicator of a successful college
+set.seed (1000)
+pca<- prcomp(training_data)
 
-summary(pcr_model)
-validationplot(pcr_model)
+#note that the first component contains all the variance pca this means all the variance in the data is explained in the first principal component
+plot(pca, type = "l")
+summary(pca)
+
+
+
+#for principal component regression
+require(pls) 
+
+set.seed (1000)
+pcr_model <- pcr(UNEMP_RATE~., data = pca, scale = TRUE, validation = "CV")
+
+ncomp_pcr <- which(pcr_model$validation$PRESS == min(pcr_model$validation$PRESS)) #selects components with best model
+pcr_coef <- coef(pcr_model)
+save(pcr_coef, file = "../../data/pcr_model.RData")
+
+
 predplot(pcr_model)
 coefplot(pcr_model)
 
+#Adding Histograms to Images
+png('../../images/CV_Errors_pcr.png')
+validationplot(pcr_model, val.type = "MSEP")
+dev.off()
 
-#now testing our model on the test-data
 
-
+#Test Data
 test_data <- read.csv("../../data/test_data.csv")
 
 #need to figure out how many components, look at validation plot
 pcr_pred <- predict(pcr_model, test_data, ncomp = 90)
+response <- test_data["UNEMP_RATE"]
+response <- as.matrix(response)
 
 #error, test accuracy
-mse_pca <- mean((pcr_pred - test_data["UNEMP_RATE"])^2)
+source("../functions/mse_function.R")
+MSE_pcr = MSE(pcr_pred, response)
+save(MSE_pcr, file = "../../data/MSE_pcr.RData")
 
 
+#saving  the important stuff
+sink(file = "../../data/pcr_model.txt")
+print("The PCR model")
+pcr_coef
+pcr_pred("applied predictors")
+test_pcr
+print("The PCR MSE")
+MSE_pcr
+print("The coefficients on the full dataset")
+sink()
 
-
-#LN_MEDIAN_HH_INC - Log of the median household income
-
-#Other data we considered trying to model
-#MN_EARN_WNE_P10 - Mean earnings of students working and not enrolled 10 years after entry
-#GT_25K_P7 Share of students earning over $25,000/year (threshold earnings) 7 years after entry
-#GT_25K_P6 Share of students earning over $25,000/year (threshold earnings) 6 years after entry
-#MN_EARN_WNE_INC3_P10 Mean earnings of students working and not enrolled 10 years after entry in the highest income tercile $75,001+
