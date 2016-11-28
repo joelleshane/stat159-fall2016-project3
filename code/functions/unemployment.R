131e1f61c3fec9d5e116229b2f55091c44bd39c9
Joelle Shane <joelle.shane@gmail.com>
2016-11-27 20:19:05 -0800

    Functions: adding unemployment function

---
+++ code/functions/unemployment.R
@@ -0,0 +1,6 @@
+unemployment_six_years <- function(dataset) {
+  sub <- subset(dataset, is.na(dataset$COUNT_NWNE_P6) != TRUE & is.na(dataset$COUNT_WNE_P6) != TRUE)
+  NW <- as.vector(sub$COUNT_NWNE_P6)
+  W <- as.vector(sub$COUNT_WNE_P6)
+  tot <- NW + W 
+  return (NW / tot) }
