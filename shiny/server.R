66e3551785f186183fcc5e5c085aa442bb817f2c
Bryan Alcorn <alcorn4cal@berkeley.edu>
2016-12-02 01:51:30 -0800

    SHINY: added plots for all three models and just need a table of MSE now

--- shiny/server.R
+++ shiny/server.R
@@ -4,16 +4,20 @@ library(shiny)
 shinyServer(function(input, output) {
   
   output$modelPlot1 <- renderPlot({
-    model_load <- paste("../data/",input$Model, ".RData", sep = "")
+    model_load <- paste("../data/",input$Model, "_model.RData", sep = "")
     load(model_load)
-    model_var = as.symbol(input$Model)
-    validationplot(eval(model_var))
+    model <- paste(input$Model,"_model", sep = "")
+    model_var = as.symbol(model)
+    plot(eval(model_var))
   })
   output$modelPlot2 <- renderPlot({
-    model_load <- paste("../data/",input$Model, ".RData", sep = "")
+    model_load <- paste("../data/",input$Model, "_model.RData", sep = "")
     load(model_load)
-    model_var = as.symbol(input$Model)
-    plot(eval(model_var))
+    model <- paste(input$Model,"_model", sep = "")
+    model_var = as.symbol(model)
+    if(model == "pcr_model"){
+      validationplot(eval(model_var))
+    }
   })
   
   output$MSE_combined <- renderPlot({
