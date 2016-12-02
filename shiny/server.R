169ba77a405ac42e0d5b0e0c1a11b8ffcb40a5f3
Bryan Alcorn <alcorn4cal@berkeley.edu>
2016-12-02 01:16:10 -0800

    Misc: did some data analysis and added data cleaning and data seperation to makefile

--- shiny/server.R
+++ shiny/server.R
@@ -3,15 +3,6 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
-  # Expression that generates a histogram. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
-  
-  
-  
   output$modelPlot1 <- renderPlot({
     model_load <- paste("../data/",input$Model, ".RData", sep = "")
     load(model_load)
