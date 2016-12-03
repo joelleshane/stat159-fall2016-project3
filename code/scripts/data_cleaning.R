bdad32bda62546f1b0a33f09584beb71d9fea26b
Todd Vogel <tjvogel@berkeley.edu>
2016-12-02 19:50:08 -0800

    data cleaning: pushing inclusion of short data to master

--- code/scripts/data_cleaning.R
+++ code/scripts/data_cleaning.R
@@ -7,6 +7,10 @@ for (i in 1:ncol(data_2006)) {
   }
 }
 
+data_short_2006 <- data_2006[,c("UNEMP_RATE", "INEXPFTE", "TUITIONFEE_IN", "COSTT4_A", "AVGFACSAL", "PCTPELL", "C150_4", "C150_4", "C150_L4", "RET_FT4", "PELL_COMP_ORIG_YR2_RT", "PELL_COMP_ORIG_YR3_RT", "PELL_COMP_ORIG_YR4_RT", "PELL_RPY_5YR_RT", "NOPELL_RPY_5YR_RT")]
+
+write.csv(data_short_2006, file = "../../data/data_short_2006.csv")
+
 ##### remove columns with greater than 50% of data missing
 
 data_2006 <- data_2006[, colSums(is.na(data_2006)) <= .5 * nrow(data_2006)]
@@ -25,3 +29,16 @@ for(i in 1:ncol(scaled_data_2006)){
 scaled_data_2006 <- scaled_data_2006[, colSums(is.na(scaled_data_2006)) <= .5 * nrow(scaled_data_2006)]
 
 write.csv(scaled_data_2006, file = "../../data/scaled_data_2006.csv")
+
+##### scaling and mean centering short data
+
+scaled_short_data_2006 <- scale(data_short_2006, center = TRUE, scale = TRUE)
+scaled_short_data_2006 <- cbind( data_short_2006[, c(1,2)], scaled_short_data_2006[,c(-1,-2)])
+
+for(i in 1:ncol(scaled_short_data_2006)){
+  scaled_short_data_2006[is.na(scaled_short_data_2006[,i]), i] <- median(scaled_short_data_2006[,i], na.rm = TRUE)
+}
+
+scaled_short_data_2006 <- scaled_short_data_2006[, colSums(is.na(scaled_short_data_2006)) <= .5 * nrow(scaled_short_data_2006)]
+
+write.csv(scaled_short_data_2006, file = "../../data/scaled_short_data_2006.csv")
