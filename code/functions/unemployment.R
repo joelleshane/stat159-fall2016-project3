ae5d26fcfef2d243d3e85fd5b98d6ac318ad2fd3
Joelle Shane <joelle.shane@gmail.com>
2016-12-04 13:19:39 -0800

    functions: change to unemployment.R

--- code/functions/unemployment.R
+++ code/functions/unemployment.R
@@ -1,3 +1,5 @@
+# function to calculate unemployment rate
+
 unemployment_six_years <- function(dataset) {
 	dataset$COUNT_NWNE_P6 <-   as.numeric(as.character(dataset$COUNT_NWNE_P6))
   dataset$COUNT_WNE_P6 <- as.numeric(as.character(dataset$COUNT_WNE_P6))
@@ -6,3 +8,4 @@ unemployment_six_years <- function(dataset) {
   W <- as.vector(sub$COUNT_WNE_P6)
   tot <- NW + W 
   return (NW / tot) }
+ 
